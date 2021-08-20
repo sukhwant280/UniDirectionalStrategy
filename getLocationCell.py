8c9f8b0bb6660b31d7bc7cbf2315395250fb378c
sukhwant280 <86493449+sukhwant280@users.noreply.github.com>
2021-08-20 21:06:52 +0530

    Add files via upload

---
+++ getLocationCell.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+def getIndexes(dfObj, value):
+     
+    # Empty list
+    listOfPos = []
+     
+ 
+    # isin() method will return a dataframe with
+    # boolean values, True at the positions   
+    # where element exists
+    result = dfObj.isin([value])
+     
+    # any() method will return
+    # a boolean series
+    seriesObj = result.any()
+ 
+    # Get list of columns where element exists
+    columnNames = list(seriesObj[seriesObj == True].index)
+    
+    # Iterate over the list of columns and
+    # extract the row index where element exists
+    for col in columnNames:
+        rows = list(result[col][result[col] == True].index)
+ 
+        for row in rows:
+            listOfPos.append((row, col))
+             
+    # This list contains a list tuples with
+    # the index of element in the dataframe
+    return listOfPos
